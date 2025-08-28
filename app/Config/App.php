c3b6ea55bef7907375ba93b71226faa0955915fa
Aryasaleh19 <aryasaleh582@gmail.com>
2025-08-29 00:12:22 +0800

    update lagi

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'https://amsystem.test/';
+    public string $baseURL = 'https://dev-almuhajirin.mokaraja.net/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
