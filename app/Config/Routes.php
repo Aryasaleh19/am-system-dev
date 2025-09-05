4ffd4f62d8242a2130006182dfb1229d7375ad1e
Aryasaleh19 <aryasaleh582@gmail.com>
2025-09-06 00:37:02 +0800

    Update routes api

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -12,7 +12,7 @@ $routes->get('login', 'Login::index');
 $routes->post('login', 'Login::proses');
 $routes->get('logout', 'Login::logout');
 
-$routes->group('pengaturan', function($routes) {
+$routes->group('pengaturan', function ($routes) {
     $routes->get('group_modul', 'Pengaturan\Group_modul::index');
     $routes->get('group_modul/ajaxList', 'Pengaturan\Group_modul::ajaxList');
     $routes->get('group_modul/form', 'Pengaturan\Group_modul::form');
@@ -43,7 +43,7 @@ $routes->group('pengaturan', function($routes) {
     $routes->post('managemen-modul/updateStatusMapping', 'Pengaturan\ManagemenModul::updateStatusMapping');
 });
 
-$routes->group('referensi', function($routes) {
+$routes->group('referensi', function ($routes) {
     $routes->get('gedung', 'Referensi\Gedung::index');
     $routes->get('gedung/ajaxList', 'Referensi\Gedung::ajaxList');
     $routes->get('gedung/form', 'Referensi\Gedung::form');
@@ -54,11 +54,10 @@ $routes->group('referensi', function($routes) {
     $routes->get('gedung/mapingruangan/(:num)', 'Referensi\Gedung::get/$1');
     $routes->get('gedung/formMaping', 'Referensi\Gedung::formMaping');
     $routes->get('gedung/delete/(:num)', 'Referensi\Gedung::delete/$1');
-
 });
 
 // Ruangan
-$routes->group('referensi/ruangan', function($routes) {
+$routes->group('referensi/ruangan', function ($routes) {
     $routes->post('simpan', 'Referensi\Gedung::simpanRuangan');
     $routes->post('update', 'Referensi\Gedung::updateRuangan');
     $routes->get('ajaxListRuangan/(:num)', 'Referensi\Gedung::ajaxListRuangan/$1');
@@ -67,7 +66,7 @@ $routes->group('referensi/ruangan', function($routes) {
 });
 
 // Sekolah
-$routes->group('referensi/sekolah', function($routes) {
+$routes->group('referensi/sekolah', function ($routes) {
     $routes->get('/', 'Referensi\Sekolah::index');
     $routes->get('form', 'Referensi\Sekolah::form');
     $routes->post('simpan', 'Referensi\Sekolah::simpan');
@@ -77,7 +76,7 @@ $routes->group('referensi/sekolah', function($routes) {
     $routes->get('delete/(:num)', 'Referensi\Sekolah::delete/$1');
 });
 // Agama
-$routes->group('referensi/agama', function($routes) {
+$routes->group('referensi/agama', function ($routes) {
     $routes->get('/', 'Referensi\Agama::index');
     $routes->get('form', 'Referensi\Agama::form');
     $routes->post('simpan', 'Referensi\Agama::simpan');
@@ -87,7 +86,7 @@ $routes->group('referensi/agama', function($routes) {
     $routes->get('delete/(:num)', 'Referensi\Agama::delete/$1');
 });
 // Jabatan
-$routes->group('referensi/jabatan', function($routes) {
+$routes->group('referensi/jabatan', function ($routes) {
     $routes->get('/', 'Referensi\Jabatan::index');
     // halaman halaman
     $routes->get('penerimaan', 'Referensi\Jabatan::penerimaan');
@@ -112,16 +111,15 @@ $routes->group('referensi/jabatan', function($routes) {
     $routes->post('updatePenerimaan', 'Referensi\Jabatan::updatePenerimaan');
     $routes->get('ajaxListPenerimaan', 'Referensi\Jabatan::ajaxListPenerimaan');
     $routes->get('deletePenerimaan/(:num)', 'Referensi\Jabatan::deletePenerimaan/$1');
-    
+
     // pengaturan tupoksi
     $routes->get('ajaxListTupoksi', 'Referensi\Jabatan::ajaxListTupoksi');
     $routes->post('simpanTupoksiJabatan', 'Referensi\Jabatan::simpanTupoksiJabatan');
     $routes->post('updateTupoksi', 'Referensi\Jabatan::updateTupoksi');
     $routes->get('deleteTupoksi/(:num)', 'Referensi\Jabatan::deleteTupoksi/$1');
-
 });
 // Profesi
-$routes->group('referensi/profesi', function($routes) {
+$routes->group('referensi/profesi', function ($routes) {
     $routes->get('/', 'Referensi\Profesi::index');
     $routes->get('form', 'Referensi\Profesi::form');
     $routes->post('simpan', 'Referensi\Profesi::simpan');
@@ -132,7 +130,7 @@ $routes->group('referensi/profesi', function($routes) {
 });
 
 // Jenis Pegawai
-$routes->group('referensi/jenispegawai', function($routes) {
+$routes->group('referensi/jenispegawai', function ($routes) {
     $routes->get('/', 'Referensi\Jenispegawai::index');
     $routes->get('form', 'Referensi\Jenispegawai::form');
     $routes->post('simpan', 'Referensi\Jenispegawai::simpan');
@@ -142,7 +140,7 @@ $routes->group('referensi/jenispegawai', function($routes) {
     $routes->get('delete/(:num)', 'Referensi\Jenispegawai::delete/$1');
 });
 // Jenis Penerimaan
-$routes->group('referensi/jenispenerimaan', function($routes) {
+$routes->group('referensi/jenispenerimaan', function ($routes) {
     $routes->get('/', 'Referensi\JenisPenerimaan::index');
     $routes->get('form', 'Referensi\JenisPenerimaan::form');
     $routes->post('simpan', 'Referensi\JenisPenerimaan::simpan');
@@ -152,13 +150,13 @@ $routes->group('referensi/jenispenerimaan', function($routes) {
     $routes->get('delete/(:num)', 'Referensi\JenisPenerimaan::delete/$1');
 });
 
-$routes->group('api/wilayah', function($routes) {
+$routes->group('api/wilayah', function ($routes) {
     $routes->get('provinsi', 'Api\Wilayah::provinsi');
     $routes->get('kabupaten/(:num)', 'Api\Wilayah::kabupaten/$1');
     $routes->get('kecamatan/(:num)', 'Api\Wilayah::kecamatan/$1');
     $routes->get('kelurahan/(:num)', 'Api\Wilayah::kelurahan/$1');
 });
-$routes->group('api/referensi', function($routes) {
+$routes->group('api/referensi', function ($routes) {
     $routes->get('agama', 'Api\Referensi::agama');
     $routes->get('angkatan', 'Api\Referensi::angkatan');
     $routes->get('sekolah', 'Api\Referensi::sekolah');
@@ -167,39 +165,38 @@ $routes->group('api/referensi', function($routes) {
 });
 
 // Kepegawaian (pegawai)
-$routes->group('kepegawaian/pegawai', function($routes) {
+$routes->group('kepegawaian/pegawai', function ($routes) {
     $routes->get('/', 'Kepegawaian\Pegawai::index');
-    $routes->get('form', 'Kepegawaian\Pegawai::form'); 
+    $routes->get('form', 'Kepegawaian\Pegawai::form');
     $routes->post('simpan', 'Kepegawaian\Pegawai::simpan');
     $routes->post('update', 'Kepegawaian\Pegawai::update');
-    
+
     $routes->get('ajaxList', 'Kepegawaian\Pegawai::ajaxList');
     $routes->get('get/(:num)', 'Kepegawaian\Pegawai::get/$1');
     $routes->get('delete/(:num)', 'Kepegawaian\Pegawai::delete/$1');
 
     // untuk akun
-    $routes->get('formakun', 'Kepegawaian\Pegawai::formakun'); 
+    $routes->get('formakun', 'Kepegawaian\Pegawai::formakun');
     $routes->post('simpan_akun', 'Kepegawaian\Pegawai::simpan_akun');
     $routes->get('get_akun/(:num)', 'Kepegawaian\Pegawai::get_akun/$1');
 });
 // Kepegawaian (pegawai)
-$routes->group('kepegawaian/managemenakses', function($routes) {
+$routes->group('kepegawaian/managemenakses', function ($routes) {
     $routes->get('/', 'Kepegawaian\Managemenakses::index');
     $routes->get('ajaxList', 'Kepegawaian\Managemenakses::ajaxList');
-    $routes->get('formakun', 'Kepegawaian\Managemenakses::formakun'); 
+    $routes->get('formakun', 'Kepegawaian\Managemenakses::formakun');
     $routes->get('get_akun/(:num)', 'Kepegawaian\Managemenakses::get_akun/$1');
     $routes->post('save_akses', 'Kepegawaian\Managemenakses::save_akses');
 });
 // Kepegawaian (absensi)
-$routes->group('kepegawaian/absensi', function($routes) {
+$routes->group('kepegawaian/absensi', function ($routes) {
     $routes->get('/', 'Kepegawaian\Absensi::index');
-
 });
 // Kesiswaan (angkatan)
-$routes->group('kesiswaan/angkatan', function($routes) {
+$routes->group('kesiswaan/angkatan', function ($routes) {
     $routes->get('/', 'Kesiswaan\Angkatan::index');
     $routes->get('ajaxList', 'Kesiswaan\Angkatan::ajaxList');
-    $routes->get('form', 'Kesiswaan\Angkatan::form'); 
+    $routes->get('form', 'Kesiswaan\Angkatan::form');
     $routes->get('get/(:num)', 'Kesiswaan\Angkatan::get/$1');
     $routes->get('delete/(:num)', 'Kesiswaan\Angkatan::delete/$1');
     $routes->post('update', 'Kesiswaan\Angkatan::update');
@@ -207,10 +204,10 @@ $routes->group('kesiswaan/angkatan', function($routes) {
 });
 
 // Kesiswaan (siswa)
-$routes->group('kesiswaan/siswa', function($routes) {
+$routes->group('kesiswaan/siswa', function ($routes) {
     $routes->get('/', 'Kesiswaan\Siswa::index');
     $routes->get('ajaxList', 'Kesiswaan\Siswa::ajaxList');
-    $routes->get('formsiswabaru', 'Kesiswaan\Siswa::formsiswabaru'); 
+    $routes->get('formsiswabaru', 'Kesiswaan\Siswa::formsiswabaru');
     $routes->get('get/(:segment)', 'Kesiswaan\Siswa::get/$1');
     $routes->post('delete/(:segment)', 'Kesiswaan\Siswa::delete/$1');
     $routes->post('update', 'Kesiswaan\Siswa::update');
@@ -244,11 +241,10 @@ $routes->group('kesiswaan/siswa', function($routes) {
     $routes->post('savePrestasi', 'Kesiswaan\Siswa::savePrestasi');
     $routes->post('updatePrestasi', 'Kesiswaan\Siswa::updatePrestasi');
     $routes->post('deletePrestasi', 'Kesiswaan\Siswa::deletePrestasi');
-    
 });
 
 // Keuangan (jenis pengeluaran)
-$routes->group('referensi/jenispengeluaran', function($routes) {
+$routes->group('referensi/jenispengeluaran', function ($routes) {
     $routes->get('/', 'Referensi\JenisPengeluaran::index');
     $routes->get('form', 'Referensi\JenisPengeluaran::form');
     $routes->post('simpan', 'Referensi\JenisPengeluaran::simpan');
@@ -258,7 +254,7 @@ $routes->group('referensi/jenispengeluaran', function($routes) {
     $routes->get('delete/(:num)', 'Referensi\JenisPengeluaran::delete/$1');
 });
 // Keuangan (Rekening Bank)
-$routes->group('keuangan/rekeningbank', function($routes) {
+$routes->group('keuangan/rekeningbank', function ($routes) {
     $routes->get('/', 'Keuangan\RekeningBank::index');
     $routes->get('form', 'Keuangan\RekeningBank::form');
     $routes->post('save', 'Keuangan\RekeningBank::save');
@@ -268,22 +264,21 @@ $routes->group('keuangan/rekeningbank', function($routes) {
     $routes->get('delete/(:num)', 'Keuangan\RekeningBank::delete/$1');
 });
 // Keuangan (Kas Masuk)
-$routes->group('keuangan/kasmasuk', function($routes) {
+$routes->group('keuangan/kasmasuk', function ($routes) {
     $routes->get('/', 'Keuangan\KasMasuk::index');
     $routes->post('save', 'Keuangan\KasMasuk::save');
     $routes->post('getriwayat', 'Keuangan\KasMasuk::getriwayat');
     $routes->post('delete', 'Keuangan\KasMasuk::delete');
-
 });
 // Keuangan (Kas Keluar)
-$routes->group('keuangan/kaskeluar', function($routes) {
+$routes->group('keuangan/kaskeluar', function ($routes) {
     $routes->get('/', 'Keuangan\KasKeluar::index');
     $routes->post('save', 'Keuangan\KasKeluar::save');
     $routes->post('getriwayat', 'Keuangan\KasKeluar::getriwayat');
     $routes->post('delete', 'Keuangan\KasKeluar::delete');
 });
 // Keuangan (Penggajian)
-$routes->group('keuangan/penggajian', function($routes) {
+$routes->group('keuangan/penggajian', function ($routes) {
     $routes->get('getJabatanByIdPegawai', 'Keuangan\Penggajian::getJabatanByIdPegawai');
     $routes->get('getPenerimaanByIdPegawai', 'Keuangan\Penggajian::getPenerimaanByIdPegawai');
     $routes->post('save', 'Keuangan\Penggajian::save');
@@ -291,7 +286,7 @@ $routes->group('keuangan/penggajian', function($routes) {
     $routes->post('delete', 'Keuangan\Penggajian::delete');
 });
 // Keuangan (Pinjaman)
-$routes->group('keuangan/pinjaman', function($routes) {
+$routes->group('keuangan/pinjaman', function ($routes) {
     $routes->get('getCekPinjamanPegawai', 'Keuangan\Pinjaman::getCekPinjamanPegawai');
     $routes->post('savePinjaman', 'Keuangan\Pinjaman::savePinjaman');
     $routes->post('deletePinjaman', 'Keuangan\Pinjaman::deletePinjaman');
@@ -300,15 +295,14 @@ $routes->group('keuangan/pinjaman', function($routes) {
     $routes->get('getDataSetoranByIdPinjaman', 'Keuangan\Pinjaman::getDataSetoranByIdPinjaman'); //<- riwayat setoran
     $routes->post('simpanSetoranPinjaman', 'Keuangan\Pinjaman::simpanSetoranPinjaman');
     $routes->post('deleteSetoranByIdSetoran', 'Keuangan\Pinjaman::deleteSetoranByIdSetoran');
-
 });
 
 
 // Keuangan (Pemabayaran Siswa)
-$routes->group('keuangan/pembayaransiswa', function($routes) {
+$routes->group('keuangan/pembayaransiswa', function ($routes) {
     $routes->get('/', 'Keuangan\PembayaranSiswa::index');
     $routes->get('ajaxList', 'Keuangan\PembayaranSiswa::ajaxList');
-    
+
     $routes->get('getDetail/(:segment)', 'Keuangan\PembayaranSiswa::getDetail/$1');
     $routes->get('modaldetailsiswa', 'Keuangan\PembayaranSiswa::modaldetailsiswa');
     $routes->get('detail/berkas', 'Keuangan\PembayaranSiswa::detailBerkas');
@@ -317,7 +311,7 @@ $routes->group('keuangan/pembayaransiswa', function($routes) {
     $routes->get('detail/profile', 'Keuangan\PembayaranSiswa::detailProfile');
     $routes->get('detail/kartukontrol', 'Keuangan\PembayaranSiswa::kartukontrol');
     $routes->get('detail/prestasi', 'Keuangan\PembayaranSiswa::prestasi');
-    
+
     $routes->match(['post'], 'simpanPembayaran', 'Keuangan\PembayaranSiswa::simpanPembayaran');
     $routes->post('hapusPembayaran', 'Keuangan\PembayaranSiswa::deletePembayaranSiswa');
     $routes->get('getRiwayatPembayaranByNis', 'Keuangan\PembayaranSiswa::getRiwayatPembayaranByNis');
@@ -328,7 +322,7 @@ $routes->group('keuangan/pembayaransiswa', function($routes) {
     $routes->get('getRiwayatPembayaranByNisInfo', 'Keuangan\PembayaranSiswa::getRiwayatPembayaranByNisInfo');
 });
 
-$routes->group('laporan/keuangan', function($routes) {
+$routes->group('laporan/keuangan', function ($routes) {
     $routes->get('/', 'Laporan\Keuangan\Lap_PembayaranSiswa::index');
     $routes->get('kartukontrol', 'Laporan\Keuangan\Lap_PembayaranSiswa::kartuKontrol');
     $routes->get('kwitansipembayaransiswa', 'Laporan\Keuangan\Lap_PembayaranSiswa::kwitansipembayaransiswa');
@@ -338,14 +332,13 @@ $routes->group('laporan/keuangan', function($routes) {
 
     // laporan harian petugas
     $routes->get('pdfLaporanHarianPetugas', 'Laporan\Keuangan\LaporanHarianPetugas::pdfLaporanHarianPetugas');
-
 });
-$routes->group('keuangan/laporankas', function($routes) {
+$routes->group('keuangan/laporankas', function ($routes) {
     $routes->get('LaporanPenerimaanKas', 'Laporan\Keuangan\LaporanKas::LaporanPenerimaanKas');
     $routes->get('LaporanPengeluaranKas', 'Laporan\Keuangan\LaporanKas::LaporanPengeluaranKas');
 });
 
-$routes->group('api/select2', function($routes) {
+$routes->group('api/select2', function ($routes) {
     $routes->get('agama', 'Api\Referensi::agama');
     $routes->get('angkatan', 'Api\Referensi::angkatan');
     $routes->get('sekolah', 'Api\Referensi::sekolah');
@@ -368,7 +361,7 @@ $routes->group('api/select2', function($routes) {
 });
 
 // Perencanaan (program)
-$routes->group('perencanaan/program', ['namespace' => 'App\Controllers\Perencanaan'], function($routes){
+$routes->group('perencanaan/program', ['namespace' => 'App\Controllers\Perencanaan'], function ($routes) {
     $routes->get('/', 'Program::index');           // halaman daftar program
     $routes->get('ajaxList', 'Program::ajaxList'); // data table ajax
     $routes->get('get/(:num)', 'Program::get/$1'); // ambil 1 program untuk edit
@@ -378,7 +371,7 @@ $routes->group('perencanaan/program', ['namespace' => 'App\Controllers\Perencana
 });
 
 // Perencanaan (kegiatan)
-$routes->group('perencanaan/kegiatan', function($routes) {
+$routes->group('perencanaan/kegiatan', function ($routes) {
     $routes->get('/', 'Perencanaan\Kegiatan::index');
     $routes->get('ajaxList', 'Perencanaan\Kegiatan::ajaxList');
     $routes->get('get/(:num)', 'Perencanaan\Kegiatan::get/$1');
@@ -389,7 +382,7 @@ $routes->group('perencanaan/kegiatan', function($routes) {
 
 
 // Perencanaan (subkegiatan)
-$routes->group('perencanaan/detail', function($routes) {
+$routes->group('perencanaan/detail', function ($routes) {
     $routes->get('/', 'Perencanaan\Detail::index');
     $routes->get('form', 'Perencanaan\Detail::form');
     $routes->post('save', 'Perencanaan\Detail::save');
@@ -409,12 +402,11 @@ $routes->group('perencanaan/detail', function($routes) {
     $routes->post('saveBelanja', 'Perencanaan\Detail::saveBelanja');
     $routes->post('updateBelanja', 'Perencanaan\Detail::updateBelanja');
     $routes->get('deleteBelanja/(:num)', 'Perencanaan\Detail::deleteBelanja/$1');
-
 });
 
 
 // Perencanaan (belanja)
-$routes->group('perencanaan/belanja', function($routes) {
+$routes->group('perencanaan/belanja', function ($routes) {
     $routes->get('/', 'Perencanaan\Belanja::index');
     $routes->get('form', 'Perencanaan\Belanja::form');
     $routes->post('simpan', 'Perencanaan\Belanja::simpan');
@@ -426,17 +418,17 @@ $routes->group('perencanaan/belanja', function($routes) {
 
 
 // API ABSEN
-$routes->group('api/absensi', function($routes) {
+$routes->group('api/absensi', function ($routes) {
     // Auth
     $routes->post('login', 'Api\Api::login');
     $routes->post('update_profile', 'Api\Api::update_profile');
     $routes->post('change_password', 'Api\Api::change_password');
 
     $routes->get('get/lembaga', 'Api\Api::get_data_lembaga');
-    
+
     $routes->post('submit', 'Api\Api::submit_absensi');
     $routes->get('get/absen', 'Api\Api::get_absen');
-    
+
     // Referensi
     $routes->get('get/agama', 'Api\Api::get_data_agama');
     $routes->get('get/pendidikan', 'Api\Api::get_data_pendidikan');
@@ -444,7 +436,7 @@ $routes->group('api/absensi', function($routes) {
     $routes->get('get/profesi', 'Api\Api::get_data_profesi');
     $routes->get('get/jenis-pegawai', 'Api\Api::get_data_jenis_pegawai');
     $routes->get('get/gedung', 'Api\Api::get_data_gedung');
-    
+
     $routes->get('get/absen-today', 'Api\Api::get_data_absen_pengaturan');
     $routes->get('get/tupoksi', 'Api\Api::get_tupoksi');
     $routes->post('submit_kinerja', 'Api\Api::submit_kinerja');
@@ -452,11 +444,4 @@ $routes->group('api/absensi', function($routes) {
     $routes->get('get/get_kinerja', 'Api\Api::get_kinerja');
     $routes->get('get/get_kinerja_by_tupoksi', 'Api\Api::get_kinerja_by_tupoksi');
     $routes->get('get/get_kinerja_bulanan', 'Api\Api::get_kinerja_bulanan');
-    
 });
-
-
-
-
-
-
